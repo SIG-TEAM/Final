2b709306c5a904210fa5e51cdef70cec074d0540
getee1411 <gentabhatara@gmail.com>
2025-04-14 16:02:43 +0700

    edit potensi desa

---
+++ app/Models/PotensiDesa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PotensiDesa extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'nama_potensi', 
+        'kategori', 
+        'deskripsi', 
+        'detail', 
+        'alamat', 
+        'desa', 
+        'latitude', 
+        'longitude',    
+        'gambar'
+    ];
+}
